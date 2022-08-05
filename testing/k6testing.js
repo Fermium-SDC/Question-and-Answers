c0ec4fcd192cbcd46ed9f4fbd8dc744aa23623ad
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-08-05 14:11:52 -0500

    k6 stuff

--- testing/k6testing.js
+++ testing/k6testing.js
@@ -1,18 +1,17 @@
 import http from 'k6/http';
 // import { sleep } from 'k6';
 
-// export const options = {
-//   stages: [
-//     { duration: '30s', target: 100 },
-//     // { duration: '30s', target: 1000 },
-//     // { duration: '30s', target: 0 },
-//   ],
-// };
-
 export const options = {
-  vus: 10,
-  duration: '5s',
+  stages: [
+    { duration: '30s', target: 100 },
+    { duration: '30s', target: 1000 },
+  ],
 };
+
+// export const options = {
+//   vus: 10,
+//   duration: '5s',
+// };
 
 export default function () {
   const product_id = Math.floor(Math.random() * (1000011 - 900000) + 900000);
