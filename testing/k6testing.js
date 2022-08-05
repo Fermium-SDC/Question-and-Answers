d8948117735a20b57a51e2321abd9b0bdbae20e6
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-08-05 09:30:44 -0500

    Prior to deployment

---
+++ testing/k6testing.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+// import { sleep } from 'k6';
+
+// export const options = {
+//   stages: [
+//     { duration: '30s', target: 100 },
+//     // { duration: '30s', target: 1000 },
+//     // { duration: '30s', target: 0 },
+//   ],
+// };
+
+export const options = {
+  vus: 10,
+  duration: '5s',
+};
+
+export default function () {
+  const product_id = Math.floor(Math.random() * (1000011 - 900000) + 900000);
+  http.get(`http://localhost:3000/qa/questions?product_id=${product_id}`);
+}
