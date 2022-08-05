034917a24412fa4ec6b6f141d916a23722fac83e
Taylor Thornton <taylorthornton.texas@gmail.com>
2022-08-05 09:34:20 -0500

    Added change in connection to server from example.env

--- server/db.js
+++ server/db.js
@@ -22,7 +22,7 @@ require('dotenv').config();
 //   logging: false,
 // });
 
-const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${process.env.cloudUser}:${process.env.cloudPW}@${process.env.cloudURL}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
   logging: false,
 });
 
